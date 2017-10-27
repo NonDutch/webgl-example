4259ef07c24d5594337a65043c28bcda7c0dd398
jedmundo <joao.a.edmundo@gmail.com>
2017-10-27 15:31:32 +0200

    added default image

--- src/App.vue
+++ src/App.vue
@@ -53,6 +53,10 @@
             }
         },
 
+        mounted() {
+            this.renderImage('public/test_image.jpg');
+        },
+
         methods: {
             showFileSelector() {
                 this.$refs.fileSelector.click();
@@ -71,25 +75,31 @@
 
             fileUpload() {
                 const reader = new FileReader();
-                const canvas = this.$refs.testCanvas;
                 reader.readAsDataURL(this.$refs.fileSelector.files[ 0 ]);
                 reader.onload = (e) => {
-                    const image = new Image();
-                    image.src = e.target.result;
+                    this.renderImage(e.target.result);
+                };
+            },
 
-                    image.onload = () => {
-                        canvas.width = image.naturalWidth;
-                        canvas.height = image.naturalHeight;
+            renderImage(imageSrc) {
+                const image = new Image();
+                const canvas = this.$refs.testCanvas;
 
-                        const gl = canvas.getContext('webgl');
-                        this.gl = gl;
+                image.src = imageSrc;
 
-                        gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+                image.onload = () => {
+                    canvas.width = image.naturalWidth;
+                    canvas.height = image.naturalHeight;
 
-                        gl.clearColor(1, 0, 0, 1);
-                        gl.clear(gl.COLOR_BUFFER_BIT);
+                    const gl = canvas.getContext('webgl');
+                    this.gl = gl;
 
-                        const vertShaderSource = `
+                    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+
+                    gl.clearColor(1, 0, 0, 1);
+                    gl.clear(gl.COLOR_BUFFER_BIT);
+
+                    const vertShaderSource = `
                     attribute vec2 position;
 
                     varying vec2 texCoords;
@@ -101,7 +111,7 @@
                     }
                 `;
 
-                        const fragShaderSource = `
+                    const fragShaderSource = `
                     precision highp float;
 
                     varying vec2 texCoords;
@@ -119,60 +129,57 @@
                     }
                 `;
 
-                        const vertShader = gl.createShader(gl.VERTEX_SHADER);
-                        const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+                    const vertShader = gl.createShader(gl.VERTEX_SHADER);
+                    const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
 
-                        gl.shaderSource(vertShader, vertShaderSource);
-                        gl.shaderSource(fragShader, fragShaderSource);
+                    gl.shaderSource(vertShader, vertShaderSource);
+                    gl.shaderSource(fragShader, fragShaderSource);
 
-                        gl.compileShader(vertShader);
-                        gl.compileShader(fragShader);
+                    gl.compileShader(vertShader);
+                    gl.compileShader(fragShader);
 
-                        const program = gl.createProgram();
-                        gl.attachShader(program, vertShader);
-                        gl.attachShader(program, fragShader);
+                    const program = gl.createProgram();
+                    gl.attachShader(program, vertShader);
+                    gl.attachShader(program, fragShader);
 
-                        gl.linkProgram(program)
+                    gl.linkProgram(program)
 
-                        gl.useProgram(program);
+                    gl.useProgram(program);
 
-                        const brightnessLocation = gl.getUniformLocation(program, 'brightness');
-                        this.brightnessLocation = brightnessLocation;
-                        const temperatureLocation = gl.getUniformLocation(program, 'temperature');
-                        this.temperatureLocation = temperatureLocation;
+                    this.brightnessLocation = gl.getUniformLocation(program, 'brightness');
+                    this.temperatureLocation = gl.getUniformLocation(program, 'temperature');
 
-                        const vertices = new Float32Array([
-                            -1, -1,
-                            -1, 1,
-                            1, 1,
+                    const vertices = new Float32Array([
+                        -1, -1,
+                        -1, 1,
+                        1, 1,
 
-                            -1, -1,
-                            1, 1,
-                            1, -1,
-                        ]);
+                        -1, -1,
+                        1, 1,
+                        1, -1,
+                    ]);
 
-                        const vertexBuffer = gl.createBuffer();
-                        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-                        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+                    const vertexBuffer = gl.createBuffer();
+                    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+                    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
-                        const positionLocation = gl.getAttribLocation(program, 'position');
+                    const positionLocation = gl.getAttribLocation(program, 'position');
 
-                        gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
-                        gl.enableVertexAttribArray(positionLocation);
+                    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+                    gl.enableVertexAttribArray(positionLocation);
 
-                        const texture = gl.createTexture();
-                        gl.activeTexture(gl.TEXTURE0);
-                        gl.bindTexture(gl.TEXTURE_2D, texture);
-                        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+                    const texture = gl.createTexture();
+                    gl.activeTexture(gl.TEXTURE0);
+                    gl.bindTexture(gl.TEXTURE_2D, texture);
+                    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 
-                        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-                        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-                        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-                        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
 
-                        gl.drawArrays(gl.TRIANGLES, 0, 6);
-                    };
+                    gl.drawArrays(gl.TRIANGLES, 0, 6);
                 };
             }
         },
